1e86c4cbecca880e6c0a309775293dee5d76da42
Hieu Lai <hieuqlai@gmail.com>
2018-04-15 16:05:34 +1000

    customised topnav

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-
+/* Toggle between adding and removing the "responsive" class to topnav when the
+   user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
+    var y = document.getElementById("rTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
+        y.className += " responsive";
     } else {
         x.className = "topnav";
+        y.className = "header-right"
     }
 }
