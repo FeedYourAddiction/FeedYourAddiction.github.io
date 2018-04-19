0b33b4ab71170e5dacf57c96ce054e8abd934bbc
Hieu Lai <hieuqlai@gmail.com>
2018-04-19 22:48:39 +1000

    topnav toggles color

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function myFunction() {
         y.className = "navRight"
     }
 }
+
+/* Toggle between a solid and transparent color to topnav when the user scrolls
+   to the top */
+function checkTop() {
+  var x = document.getElementById("myTopnav");
+
+  if(document.body.scrollTop !== 0) {
+    x.style.backgroundColor =  "#333";
+  }
+  else {
+    x.style.backgroundColor = "transparent";
+  }
+}
