319ea0b8713abdd6063d583070faeed99a6fb4f7
Hieu Lai <hieuqlai@gmail.com>
2018-05-17 15:00:43 +1000

    edit content

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ var i = 0;
 var j = 0;
 var txt = ['enjoys long walks on the beach.',
            'appreciates the occassional meme.',
-           'loves having fun.',
+           'creating apps.',
            'enjoys learning code!'];
 var speed = 50;
 var timer;
